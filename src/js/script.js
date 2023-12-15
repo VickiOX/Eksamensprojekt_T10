5a30fccc8cb9caeff6967c20e021911cb9fc12fc
Krestine H <krha0001@stud.kea.dk>
2023-12-15 12:22:51 +0100

    a lot of small updates

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,25 @@ burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("active");
   dropDown.classList.toggle("active");
 });
+
+// Kurv opdate
+
+const plusBtn = document.querySelector(".plus");
+const minusBtn = document.querySelector(".minus");
+
+let products = 0;
+
+document.querySelector("#number").textContent = products;
+
+plusBtn.addEventListener("click", plusProduct);
+
+function plusProduct() {
+  products = products + 1;
+  return products;
+}
+
+// minusBtn.addEventListener("click", minusProduct);
+
+// function minusProduct() {
+//   products = products - 1;
+// }
